60dbe10de396e30d0691f7615b82104905b27ee8
Laird Tuttle <hey@thisis.lt>
2015-04-30 18:01:01 -0700

    Chains moves

--- board.rb
+++ board.rb
@@ -5,14 +5,12 @@ require 'unicode'
 class Board
   attr_accessor :grid
 
-  WHITE_PIECE = "\u2687"
-  WHITE_KING =  "\u2654"
-  BLACK_PIECE = "\u2689"
-  BLACK_KING =  "\u265a"
+  PIECE = "\u25cf"
+  KING =  "\u265a"
 
-  def initialize
-    @grid = Array.new(8) { Array.new(8) }
-    setup_board
+  def initialize(setup = true)
+    @grid = []
+    setup_board if setup
   end
 
   def [](pos)
@@ -25,13 +23,34 @@ class Board
     @grid[row][col] = piece
   end
 
-  def display
-    system 'clear'
+  def deep_dup
+    duped_board = Board.new(false)
+    @grid.each do |row|
+      duped_row = []
+      row.each do |square|
+        unless square.nil?
+          duped_row << Piece.new(duped_board, square.color,
+                                 square.pos.dup, square.king)
+        else
+          duped_row << nil
+        end
+      end
+      duped_board.grid << duped_row.dup
+    end
+
+    duped_board
+  end
+
+
+  def display(cursor)
+    #system 'clear'
     puts "   0  1  2  3  4  5  6  7 "
     @grid.each_with_index do |row,idx_x|
       print "#{idx_x} "
       row.each_with_index do |square,idx_y|
-        if square.nil?
+        if cursor == [idx_x,idx_y]
+          display_cursor(square)
+        elsif square.nil?
           display_squares(square,idx_x,idx_y)
         elsif square.king
           display_kings(square)
@@ -44,19 +63,34 @@ class Board
     puts ''
   end
 
+  def display_cursor(square)
+    if square.nil?
+      print "   ".on_light_yellow
+    elsif square.color == :black && square.king == true
+      print " #{KING} ".on_light_yellow
+    elsif square.color == :black
+      print " #{PIECE} ".on_light_yellow
+    elsif square.king == true
+      print " #{KING} ".colorize(:white).on_light_yellow
+    else
+      print " #{PIECE} ".colorize(:white).on_light_yellow
+    end
+  end
+
+
   def display_kings(square)
     if square.color == :black
-      print " #{BLACK_KING} ".on_light_red
+      print " #{KING} ".on_light_red
     else
-      print " #{WHITE_KING} ".on_light_red
+      print " #{KING} ".colorize(:white).on_light_red
     end
   end
 
   def display_pieces(square)
     if square.color == :black
-      print " #{BLACK_PIECE} ".on_light_red
+      print " #{PIECE} ".on_light_red
     else
-      print " #{WHITE_PIECE} ".on_light_red
+      print " #{PIECE} ".colorize(:white).on_light_red
     end
   end
 
@@ -70,6 +104,7 @@ class Board
 
   def setup_board
     color = :white
+    @grid = Array.new(8) { Array.new(8) }
     @grid.each_with_index do |row, idx_x|
       color = :black if idx_x == 4
       next if idx_x.between?(3,4)
@@ -82,8 +117,5 @@ class Board
       end
     end
   end
-end
 
-board = Board.new
-board[[2,2]].perform_slide([3,3])
-board.display
+end
--- pieces.rb
+++ pieces.rb
@@ -7,11 +7,11 @@ class Piece
 
   ALL_MOVES = [[1,1],[1,-1],[-1,-1],[-1,1]]
 
-  def initialize(board, color, pos)
+  def initialize(board, color, pos, king = false)
     @grid = board
     @color = color
     @pos = pos
-    @king = false
+    @king = king
   end
 
   def inspect
@@ -38,12 +38,14 @@ class Piece
     jumped = [(endpoint[0] + @pos[0])/2,(endpoint[1] + @pos[1])/2]
     if !grid[jumped].nil? && grid[jumped].color != self.color
       if move_diff.any? { |x,y| [@pos[0] + x, @pos[1] + y] == jumped }
-        grid[self.pos] = nil
-        grid[jumped] = nil
-        grid[endpoint] = self
-        pos = endpoint
-      else
-        false
+        if grid[endpoint].nil?
+          grid[@pos] = nil
+          grid[jumped] = nil
+          grid[endpoint] = self
+          @pos = endpoint
+        else
+          false
+        end
       end
     end
   end
@@ -52,17 +54,34 @@ class Piece
   end
 
   def perform_moves!(moves)
-  end
-
-  def perform_slide(endpoint)
-    if move_diff.any? { |x,y| [@pos[0] + x, @pos[1] + y] == endpoint }
-      grid[self.pos] = nil
-      grid[endpoint] = self
-      @pos = endpoint
+    moved = true
+    if moves.length == 2
+      moved = self.perform_slide(moves.last)
+      if moved == false
+        moved = self.perform_jump(moves.last)
+      end
     else
-      false
+      moves.drop(1).each_with_index do |move,idx|
+        grid[moves[idx]].perform_jump(move)
+      end
     end
   end
 
+  def perform_slide(endpoint)
+    if grid[endpoint].nil?
+      if move_diff.any? { |x,y| [@pos[0] + x, @pos[1] + y] == endpoint }
+        grid[self.pos] = nil
+        grid[endpoint] = self
+        @pos = endpoint
+      else
+        false
+      end
+    end
+  end
+
+  def valid_move_seq?(moves)
+    duped_board = @grid.deep_dup
+    duped_board[moves.first].perform_moves!(moves)
+  end
 
 end
