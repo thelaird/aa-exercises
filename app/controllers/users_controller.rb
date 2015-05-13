ed40d5550576fd3aeb5f14160c0c089fc340310f
Laird Tuttle <hey@thisis.lt>
2015-05-13 13:19:02 -0700

    Basic signup through Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to cats_url
+    else
+      flash.new[:errors] = @user.errors.full_messages
+    end
+
+    render :new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ NinetyNineCatsDay1::Application.routes.draw do
     post "deny", on: :member
   end
 
+  resources :users, only: [:create, :new]
+
   root to: redirect("/cats")
 end
