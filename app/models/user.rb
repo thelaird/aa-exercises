c4c5f6fa2e3bd591253b0032c9e7feb019cc1abb
Laird Tuttle <hey@thisis.lt>
2015-05-13 11:47:59 -0700

    Create User migration and model with basic auth methods

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by(username: username)
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    new_token = SecureRandom::urlsafe_base64
+    new_token if self.update(session_token: new_token)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030172156) do
+ActiveRecord::Schema.define(version: 20150513182954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,5 +36,16 @@ ActiveRecord::Schema.define(version: 20141030172156) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
