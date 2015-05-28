99b78b17ce300d3b3306452d5471304a8ad038cc
Laird Tuttle <hey@thisis.lt>
2015-05-28 11:55:34 -0700

    Add content tabs

--- tabs.js
+++ tabs.js
@@ -1,7 +1,22 @@
-$.Tabs = function (el) { ... };
+$.Tabs = function (el) {
+  this.$el = $(el);
+  this.$contentTabs = $(this.$el.attr("data-content-tabs"));
+  this.$activeTab = this.$contentTabs.find(".active");
+  this.$activeLink = this.$el.find(".active");
+  this.$el.on('click', 'a', this.clickTab.bind(this));
+ };
 
 $.fn.tabs = function () {
   return this.each(function () {
     new $.Tabs(this);
   });
+};
+
+
+$.Tabs.prototype.clickTab = function (event) {
+  this.$activeTab.removeClass('active');
+  this.$activeLink.removeClass('active');
+  this.$activeLink = $(event.currentTarget).addClass('active');
+  this.$activeTab = $(this.$activeLink.attr("href")).addClass('active');
+
 };
