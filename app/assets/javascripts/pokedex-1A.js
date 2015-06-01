3505ed0cb47ada303cf6e6448b8325c0e74a4ead
Laird Tuttle <hey@thisis.lt>
2015-06-01 11:42:04 -0700

    add pokemon to list, refresh

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,14 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $pokeLi = $("<li>").addClass("poke-list-item");
+  $pokeLi.text(pokemon.get('name') + " - " + pokemon.escape('poke_type'));
+  this.$pokeList.append($pokeLi);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function () {
+  var that = this;
+  this.pokes.fetch({
+    success: function () {
+      that.pokes.each( function (poke) { that.addPokemonToList(poke); });
+    }
+  });
 };
